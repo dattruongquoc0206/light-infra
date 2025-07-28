f77bd5a505ef5055a55f4b855f8f61f9875d26df
Truong Quoc Dat <100710884+dattruongquoc0206@users.noreply.github.com>
2025-07-28 21:47:52 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,13 +8,18 @@ resource "google_container_cluster" "primary" {
   name     = var.cluster_name
   location = var.region
 
-  remove_default_node_pool = true
+  remove_default_node_pool = False
+  initial_node_count       = 1
 
   network    = "default"
   subnetwork = "default"
-  lifecycle {
-    ignore_changes = [initial_node_count]
-  }
+  node_config {
+    disk_size_gb = 10
+    disk_type    = "pd-standard"
+    machine_type = "e2-medium"
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform"
+    ]
 }
 
 resource "google_container_node_pool" "primary_nodes" {
