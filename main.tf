e2320f28e97e16bf7a57f4d5b1c2186fac46e21f
twelfthywn <tqd.dev@gmail.com>
2025-08-06 19:02:52 +0700

    typo comment

--- main.tf
+++ main.tf
@@ -1,9 +1,10 @@
-/*
 provider "google" {
   project = var.project_id
   region  = var.region
   credentials = file(var.credentials_file)
 }
+/*
+
 
 resource "google_container_cluster" "primary" {
   name     = var.cluster_name
@@ -17,11 +18,10 @@ resource "google_container_cluster" "primary" {
   subnetwork = "default"
   node_config {
   disk_size_gb = 70
-/*
+
    
     machine_type = "e2-medium"
-*/
-    oauth_scopes = [
+*    oauth_scopes = [
       "https://www.googleapis.com/auth/cloud-platform"
     ]
     }
