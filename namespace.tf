39850354370034fccf74955d6d80e4c85cf1ec49
twelfthywn <tqd.dev@gmail.com>
2025-08-08 00:25:29 +0700

    try get token straight from k8s

--- namespace.tf
+++ namespace.tf
@@ -1,5 +1,7 @@
 provider "kubernetes" {
-  config_path = "kubeconfig.yaml"
+  host                   = "https://${google_container_cluster.primary.endpoint}"
+  token                  = data.google_client_config.default.access_token
+  cluster_ca_certificate = base64decode(google_container_cluster.primary.master_auth[0].cluster_ca_certificate)
 }
 
 resource "kubernetes_namespace" "api" {
