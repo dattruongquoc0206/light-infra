231bcd7689aad3e5452d3e090c9f6b5327c3e7f8
twelfthywn <tqd.dev@gmail.com>
2025-08-08 12:14:00 +0700

    use secret 3rd party

--- namespace.tf
+++ namespace.tf
@@ -1,7 +1,5 @@
 provider "kubernetes" {
-  host                   = "https://${google_container_cluster.primary.endpoint}"
-  token                  = data.google_client_config.default.access_token
-  cluster_ca_certificate = base64decode(google_container_cluster.primary.master_auth[0].cluster_ca_certificate)
+  config_path = "kubeconfig.yaml"
 }
 
 resource "kubernetes_namespace" "api" {
