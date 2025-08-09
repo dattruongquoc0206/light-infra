b083c0c468db7ceded05f8ef06af28a03d43cb91
twelfthywn <tqd.dev@gmail.com>
2025-08-09 16:25:23 +0700

    add cicd_nodepool

--- node_pool.tf
+++ node_pool.tf
@@ -29,3 +29,27 @@ resource "google_container_node_pool" "api_pool" {
     auto_upgrade = true
   }
 }
+
+resource "google_container_node_pool" "cicd_nodepool" {
+  name       = "cicd-nodepool"
+  cluster    = google_container_cluster.primary.name    
+  location   = "asia-southeast1-a"   
+  node_count = 1
+
+  node_config {
+    machine_type = "e2-medium"
+    disk_size_gb = 20
+    labels = {
+      role = "cicd"
+    }
+    taint {
+      key    = "namespace"
+      value  = "cicd_nodepool"
+      effect = "NO_SCHEDULE"
+    }
+
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform"
+    ]
+  }
+}
